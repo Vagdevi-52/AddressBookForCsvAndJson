46ebad0a5b35324b8bcdab6d045491163c5ed4e6
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-23 10:42:43 -0700

    Added: Program To Read Or Write The Address Book With Persons Contact As Csv File

---
+++ src/test/java/AddressBookForCsvAndJson/TestAddressBookCSV.java
@@ -0,0 +1,27 @@
+package AddressBookForCsvAndJson;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class TestAddressBookCSV
+{
+
+	@Test
+	public void given3ContactsWhenWrittenToCSVFileShouldMatchContactEntries() //csv file.
+	{
+		Contacts jeff=new Contacts("Amedkar","gandhji","Ame@gmail.com","San Francisco","Address","189675","California","+23-1234567890");
+		Contacts mark=new Contacts("prabhas","nehru","prabhas@gmail.com","New York ","Address","016483","New York","+12-9874563210");
+		Contacts satya=new Contacts("Satya", "Nadela","satya@gmail.com","Los Angeles","Address","987324","California","+11-5463217890");
+		List<Contacts> contacts=Arrays.asList(new Contacts[] {jeff, mark, satya});
+		AddressBookCSV addressBookCSVIOService=new AddressBookCSV("addressCsvTest.csv");
+		addressBookCSVIOService.writeContactDetailsInAFile(contacts);
+		List<Contacts> readContacts=addressBookCSVIOService.readAddressBookFromAFile();
+		assertEquals(jeff.toString(),readContacts.get(0).toString());
+		assertEquals(mark.toString(),readContacts.get(1).toString());
+		assertEquals(satya.toString(),readContacts.get(2).toString());
+	}
+}
